70afb09dce8faa78ede65dd833b8aa83f3d8142f
Jericho Ortiz <jortiz180@cnm.edu>
2021-07-19 08:32:58 -0600

    snap challenge updates

---
+++ js-data-types-arrays-objects.js
@@ -0,0 +1,23 @@
+console.log('Hello World')
+let name = 'Sue'
+console.log(name)
+let age = 18 + 1
+console.log(age)
+
+// array
+let people = ['Marty', 'Anais', 'Jonathan', 'Andrew']
+console.log(people)
+let ages = [4,5,9,2]
+console.log(ages)
+console.log(people[1])
+
+// objects
+let person1 = {
+    name: 'Anais',
+    status: 'bootcamper',
+    hobby: 'cooking',
+    code: function() {console.log('develop website')}
+}
+console.log(person1)
+console.log(person1.status)
+person1.code()
---
+++ snap-01/snap1.js
@@ -0,0 +1,26 @@
+// Function #1 Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
+function multiplyTwoNumbers(number1, number2) {
+
+    console.log("first Number", number1)
+    console.log("Second Number", number2)
+    console.log(result)
+    if(number1 === 20) {
+        var result = number1 * number2
+    }
+    return result
+}
+let firsNumber = 20
+let secondNumber = 5
+multiplyTwoNumbers(firsNumber, secondNumber)
+
+
+// Function #2
+// Take two strings as arguments and return that has them concatenated.
+const value = "A string"
+"I am" + value +" " + "isnt javascript fun"
+
+function combineTwoStrings(string1, string2) {
+    return string1 + string2
+}
+
+console.log(combineTwoStrings("Javascript is to carpet as", "Java is to car"))
