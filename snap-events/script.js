a5b5547a5f62b46a90a7edf770fdd78685fb4ccb
Jericho Ortiz <jortiz180@cnm.edu>
2021-07-30 08:57:56 -0600

    added JS Events Snap-Challenge. Attributes

---
+++ snap-events/script.js
@@ -0,0 +1,9 @@
+function handleClick() {
+	const button = document.getElementById("target")
+
+	if(button.style.backgroundColor ==="blue") {
+		button.style.backgroundColor = ""
+	}	else {
+			button.style.backgroundColor = "blue"
+		}
+	}
