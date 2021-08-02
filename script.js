355dc85c1e9bfb91278eb8dc7b77ee262097b50b
Jericho Ortiz <jortiz180@cnm.edu>
2021-08-02 08:42:26 -0600

    added new code along > Using the foreach loop display this array of text to the DOM.`

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 // ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', 'Sarah']
 //
 // Hint: Each array element must appear in it's own html tag in the browser. Extra Credit: use map or reduce.
+// Extra Credit: use map or reduce
 
 const names = ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', 'Sarah']
 
 let target = document.getElementById('target')
-
+target.innerText = 'We changed the text.'
 console.log(target)
+
+names.forEach(name=>console.log('name'))
